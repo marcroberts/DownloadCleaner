98e626c63fc99e874720d754c2830e70f1622d5b
Marc Roberts <marc@neutroncreations.com>
2018-05-22 16:34:18 +0100

    stub out docs

--- DownloadCleaner/AppDelegate.swift
+++ DownloadCleaner/AppDelegate.swift
@@ -10,52 +10,14 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    let fileProperties = Set(arrayLiteral: URLResourceKey.localizedNameKey, URLResourceKey.creationDateKey, URLResourceKey.contentModificationDateKey, URLResourceKey.localizedTypeDescriptionKey, URLResourceKey.isDirectoryKey)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let activity = NSBackgroundActivityScheduler(identifier: "DownloadCleaner.DeleteTask")
-        activity.repeats = true
-        activity.interval = 60 * 60
-        activity.tolerance = 10 * 60
-        
-        activity.schedule() { (completion: NSBackgroundActivityScheduler.CompletionHandler) in
-            // Perform the activity
-            completion(NSBackgroundActivityScheduler.Result.finished)
-        }
-        
-        let fileManager = FileManager.default
-        let downloadsURL = fileManager.urls(for: .downloadsDirectory, in: .userDomainMask).first!
-        do {
-            let enumerator = try fileManager.contentsOfDirectory(at: downloadsURL, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles] )
-            var deleted = 0
-            let oldest = Date.init(timeIntervalSinceNow: -30*86400) // 30 days in seconds
-            
-            for case let fileURL in enumerator {
-                let resourceValues = try fileURL.resourceValues(forKeys: fileProperties)
-                if (oldest > resourceValues.creationDate!) {
-                    deleted += 1
-                }
-            }
-            print("Deleted \(deleted) files odler than \(oldest)")
-            
-        } catch {
-            print("Error while enumerating files \(downloadsURL): \(error.localizedDescription)")
-        }
         
         
-        
-        
-        
-        let files =  fileManager.urls(for: .downloadsDirectory, in: .userDomainMask).first! as NSURL
-
-
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
--- DownloadCleaner/StatusMenuController.swift
+++ DownloadCleaner/StatusMenuController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018. All rights reserved.
 //
 
+import os.log
 import Cocoa
 
 extension NSImage.Name {
@@ -14,14 +15,59 @@ extension NSImage.Name {
 
 class StatusMenuController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var lastRan: NSMenuItem!
     
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    let fileProperties = Set(arrayLiteral: URLResourceKey.localizedNameKey, URLResourceKey.creationDateKey, URLResourceKey.contentModificationDateKey, URLResourceKey.localizedTypeDescriptionKey, URLResourceKey.isDirectoryKey)
     
     override func awakeFromNib() {
         let icon = NSImage(named: .statusIcon)
         icon?.isTemplate = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
+        
+        trashFiles()
+        
+        let activity = NSBackgroundActivityScheduler(identifier: "DownloadCleaner.DeleteTask")
+        activity.repeats = true
+        activity.interval = 60 * 60
+        activity.tolerance = 10 * 60
+        
+        activity.schedule() { (completion: NSBackgroundActivityScheduler.CompletionHandler) in
+            
+            self.trashFiles()
+            
+            completion(NSBackgroundActivityScheduler.Result.finished)
+        }
+    }
+    
+    private func trashFiles() {
+        let fileManager = FileManager.default
+        let downloadsURL = fileManager.urls(for: .downloadsDirectory, in: .userDomainMask).first!
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM dd, HH:mm"
+        
+        do {
+            let enumerator = try fileManager.contentsOfDirectory(at: downloadsURL, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles] )
+            var deleted = 0
+            let oldest = Date.init(timeIntervalSinceNow: -30*86400) // 30 days in seconds
+            
+            for case let fileURL in enumerator {
+                let resourceValues = try fileURL.resourceValues(forKeys: self.fileProperties)
+                if (oldest > resourceValues.creationDate!) {
+                    try fileManager.trashItem(at: fileURL, resultingItemURL: nil)
+                    deleted += 1
+                }
+            }
+            
+            NSLog("Deleted %d files older than %@", deleted, oldest.description)
+            
+            lastRan.title = "Last ran: \(dateFormatter.string(from: Date.init()))"
+            
+        } catch {
+            NSLog("Error while enumerating files (%@): %@", downloadsURL.description, error.localizedDescription)
+        }
     }
     
     @IBAction func quitClicked(sender: NSMenuItem) {
