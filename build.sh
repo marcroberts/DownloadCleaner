8de35c7edee589d82d864d8d3e8cf61f0eb5cb58
Marc Roberts <marc@neutroncreations.com>
2018-05-23 09:13:17 +0100

    update build script to generate an installer and app bundle

--- build.sh
+++ build.sh
@@ -5,11 +5,22 @@ INFOPLIST_FILE="Info.plist"
 
 CFBundleVersion=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
 
-rm -rf Archive/*
-rm -rf Product/*
+rm -rf build/*
 
 xcodebuild clean -workspace $PROJECT_NAME.xcworkspace -configuration Release -scheme $PROJECT_NAME
 
-xcodebuild archive -workspace $PROJECT_NAME.xcworkspace -scheme $PROJECT_NAME -archivePath Archive/$PROJECT_NAME.xcarchive
+xcodebuild archive -workspace $PROJECT_NAME.xcworkspace -scheme $PROJECT_NAME -archivePath build/$PROJECT_NAME.xcarchive
 
-ditto -c -k --sequesterRsrc --keepParent Archive/$PROJECT_NAME.xcarchive/Products/Applications/$PROJECT_NAME.app "Product/$PROJECT_NAME.v${CFBundleVersion}.zip"
+mkdir -p build/App
+mv build/$PROJECT_NAME.xcarchive/Products/Applications/$PROJECT_NAME.app build/App/
+
+echo "Runing pkgbuild. Note you must be connected to Internet for this to work as it"
+echo "has to contact a time server in order to generate a trusted timestamp. See"
+echo "man pkgbuild for more info under SIGNED PACKAGES."
+pkgbuild --identifier "info.marcroberts.DownloadCleaner" \
+    --component "build/App/$PROJECT_NAME.app" \
+    --install-location /Applications \
+    --version $CFBundleVersion \
+    "build/$PROJECT_NAME.pkg"
+
+ditto -c -k --sequesterRsrc --keepParent build/App/$PROJECT_NAME.app "build/$PROJECT_NAME.v${CFBundleVersion}.zip"
